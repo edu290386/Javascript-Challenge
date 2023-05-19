3b22db722b61cea58f3306427285fe5db0fc2e2b
edu290386 <eduardo.torres@outlook.com>
2023-05-18 19:11:27 -0500

    auto text

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const btn = document.getElementById("btn");
 const container = document.getElementById("container");
+const message = "Culture                 Empowerment         Awareness         Environment          ";
+const container2 = document.getElementById("container2");
+
 btn.addEventListener("click", () => {
     createNotification()
 })
 
 function createNotification() {
     const notif = document.createElement("div");
-    console.log(notif)
     notif.classList.add("toast");
     notif.innerText = "Share the Wave";
     container.appendChild(notif);
@@ -14,3 +16,14 @@ function createNotification() {
         notif.remove()
     },8000);
 }
+
+let index = 0;
+function writeText(){
+    container2.innerText = message.slice(0, index);
+    index ++;
+    if (index > message.length) {
+        index = 0;
+    }
+}
+
+setInterval(writeText, 90);
