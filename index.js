ee0f312381d78a84e30ea56d8f7ec7c84a8ac291
edu290386 <eduardo.torres@outlook.com>
2023-05-20 14:16:11 -0500

    setTimeout

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const btn = document.getElementById("btn");
 const container = document.getElementById("container");
-const message = "Culture                 Empowerment         Awareness         Environment          ";
 const container2 = document.getElementById("container2");
 
 btn.addEventListener("click", () => {
@@ -17,13 +16,38 @@ function createNotification() {
     },8000);
 }
 
-let index = 0;
-function writeText(){
-    container2.innerText = message.slice(0, index);
-    index ++;
-    if (index > message.length) {
-        index = 0;
+const messages = ["honor", "fe", "lealtad"];
+
+function showVirtud(){
+    for(let i=0; i<messages.length; i++) {
+        setTimeout(function timer(){
+            const virtud = document.createElement("div");
+            virtud.classList.add("toast");
+            container.appendChild(virtud);
+            virtud.innerText = messages[i];
+            setTimeout(() => {
+                virtud.remove()
+            },9000);
+        },i*3000); 
     }
 }
 
-setInterval(writeText, 90);
+setInterval(showVirtud, 9000);
+
+function showVirtud2(){
+    for(let i=0; i<messages.length; i++) {
+        setTimeout(function timer(){
+            const virtud2 = document.createElement("div");
+            virtud2.classList.add("container2");
+            container2.appendChild(virtud2);
+            virtud2.innerText = messages[i];
+            setTimeout(() => {
+                virtud2.remove()
+            },3000);
+        },i*3000); 
+    }
+}
+
+setInterval(showVirtud2, 9000);
+
+   
